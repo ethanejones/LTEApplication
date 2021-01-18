e21b979c422a71c8d39c90de8e32b40a1dd3586a
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 07:40:05 -0600

    Updated Jenkinsfile to test using various agents. Changed build-and-deploy-image to batch script from bash.

---
+++ jenkins/scripts/build-and-deploy-image.cmd
@@ -0,0 +1,18 @@
+@echo off
+
+echo 'The following Maven command installs your Maven-built Java application'
+echo 'into the local Maven repository, which will ultimately be stored in'
+echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
+echo 'volume).'
+mvn jar:jar install:install help:evaluate -Dexpression=project.name
+
+
+echo 'The following complex command extracts the value of the <name/> element'
+echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+for /f "delims=" %%i in ('mvn help:evaluate -Dexpression=project.name -q -DforceStdout') do set NAME="%%i"
+echo %NAME%
+
+echo 'The following complex command behaves similarly to the previous one but'
+echo 'extracts the value of the <version/> element within <project/> instead.'
+for /f "delims=" %%i in ('mvn help:evaluate -Dexpression=project.version -q -DforceStdout') do set VERSION=%%i
+echo %VERSION%
