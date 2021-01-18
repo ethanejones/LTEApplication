e6b0b25244dba11ab5eb7d5df28e15ed108495e4
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 09:11:21 -0600

    Added docker build and tag calls

--- jenkins/scripts/build-and-deploy-image.cmd
+++ jenkins/scripts/build-and-deploy-image.cmd
@@ -12,7 +12,7 @@ for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.version" -q -D
 FOR /F %%i IN ('powershell -command "(get-item env:'VERSION').Value.ToLower()"') DO set DOCKER_IMAGE_TAG=%%i
 
 REM Build Docker image
-cmd /c docker build -t ethanejones/${DOCKER_IMAGE_NAME} .
+cmd /c docker build -t ethanejones/%DOCKER_IMAGE_NAME% .
 
 REM Tag Docker image
-cmd /c docker tag ethanejones/${DOCKER_IMAGE_NAME} ethanejones/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
+cmd /c docker tag ethanejones/%DOCKER_IMAGE_NAME% ethanejones/%DOCKER_IMAGE_NAME%:%DOCKER_IMAGE_TAG%
