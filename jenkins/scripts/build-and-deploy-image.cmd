106cb0d92caf46bf97ee54f3d36d5bc3098c3e06
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 09:35:25 -0600

    Added Docker push command

--- jenkins/scripts/build-and-deploy-image.cmd
+++ jenkins/scripts/build-and-deploy-image.cmd
@@ -11,6 +11,9 @@ REM Get maven project.version
 for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.version" -q -DforceStdout') do set VERSION=%%i
 FOR /F %%i IN ('powershell -command "(get-item env:'VERSION').Value.ToLower()"') DO set DOCKER_IMAGE_TAG=%%i
 
-REM Build tagged Docker image
+REM Build and tag Docker images
 cmd /c docker build -t ethanejones/%DOCKER_IMAGE_NAME%:%DOCKER_IMAGE_TAG% .
 cmd /c docker build -t ethanejones/%DOCKER_IMAGE_NAME%:latest .
+
+REM Push tagged images to DockerHub
+cmd /c docker image push --all-tags ethanejones/%DOCKER_IMAGE_NAME%
