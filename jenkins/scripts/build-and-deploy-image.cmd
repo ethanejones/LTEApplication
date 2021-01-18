06390b1f6815017c3d66bb135658f4e0bd6e8630
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 09:02:58 -0600

    Additional build script testing for grabbing project name and version

--- jenkins/scripts/build-and-deploy-image.cmd
+++ jenkins/scripts/build-and-deploy-image.cmd
@@ -1,20 +1,12 @@
 @echo off
 
-echo 'The following Maven command installs your Maven-built Java application'
-echo 'into the local Maven repository, which will ultimately be stored in'
-echo 'Jenkins''s local Maven repository (and the "maven-repository" Docker data'
-echo 'volume).'
+REM Install Maven-built Java application into the local Maven repository
 cmd /c mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
-
-echo 'The following complex command extracts the value of the <name/> element'
-echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
+REM Get maven project.name
 for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.name" -q -DforceStdout') do set NAME="%%i"
 echo %NAME%
-echo 'abc'
 
-echo 'The following complex command behaves similarly to the previous one but'
-echo 'extracts the value of the <version/> element within <project/> instead.'
+REM Get maven project.version
 for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.version" -q -DforceStdout') do set VERSION=%%i
 echo %VERSION%
-echo '123'
