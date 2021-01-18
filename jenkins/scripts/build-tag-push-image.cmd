d8377e39bab2987998d220db7b543220f1d63814
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 09:47:50 -0600

    Removed unnecessary Maven build command

--- jenkins/scripts/build-tag-push-image.cmd
+++ jenkins/scripts/build-tag-push-image.cmd
@@ -1,8 +1,5 @@
 @echo off
 
-REM Install Maven-built Java application into the local Maven repository
-cmd /c mvn jar:jar install:install help:evaluate -Dexpression=project.name
-
 REM Get maven project.name
 for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.name" -q -DforceStdout') do set NAME=%%i
 FOR /F %%i IN ('powershell -command "(get-item env:'NAME').Value.ToLower()"') DO set DOCKER_IMAGE_NAME=%%i
