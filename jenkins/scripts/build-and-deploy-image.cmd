e093bf35ab2f9d9bf2cb868074639e49f7f7dff1
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 09:09:38 -0600

    Added docker build and tag calls

--- jenkins/scripts/build-and-deploy-image.cmd
+++ jenkins/scripts/build-and-deploy-image.cmd
@@ -4,9 +4,15 @@ REM Install Maven-built Java application into the local Maven repository
 cmd /c mvn jar:jar install:install help:evaluate -Dexpression=project.name
 
 REM Get maven project.name
-for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.name" -q -DforceStdout') do set NAME="%%i"
-echo %NAME%
+for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.name" -q -DforceStdout') do set NAME=%%i
+FOR /F %%i IN ('powershell -command "(get-item env:'NAME').Value.ToLower()"') DO set DOCKER_IMAGE_NAME=%%i
 
 REM Get maven project.version
 for /f "delims=" %%i in ('mvn help:evaluate "-Dexpression=project.version" -q -DforceStdout') do set VERSION=%%i
-echo %VERSION%
+FOR /F %%i IN ('powershell -command "(get-item env:'VERSION').Value.ToLower()"') DO set DOCKER_IMAGE_TAG=%%i
+
+REM Build Docker image
+cmd /c docker build -t ethanejones/${DOCKER_IMAGE_NAME} .
+
+REM Tag Docker image
+cmd /c docker tag ethanejones/${DOCKER_IMAGE_NAME} ethanejones/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}
