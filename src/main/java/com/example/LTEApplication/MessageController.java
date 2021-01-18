5ea09c39995a1abec31a2352f4efb04e792f08df
Ethan Jones <ethanelijones@gmail.com>
2021-01-18 11:10:06 -0600

    Added missing import for Value

--- src/main/java/com/example/LTEApplication/MessageController.java
+++ src/main/java/com/example/LTEApplication/MessageController.java
@@ -2,6 +2,7 @@ package com.example.LTEApplication;
 
 import java.sql.Timestamp;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
